77a4491410ccf0005aaab7ec4711d8906d2ba015
Rob Porter <roporter@users.noreply.github.com>
2016-04-16 15:11:21 +0100

    Initial Custom Commit

--- stats.go
+++ stats.go
@@ -6,7 +6,25 @@ import (
 	"os"
 	"sync"
 	"time"
+	"github.com/zenazn/goji/web"
 )
+
+type TimeSpan struct {
+	Weeks				int
+	Days				int
+	Hours				int
+	Minutes				int
+	Seconds 			int
+}
+
+type ResponseURL struct {	
+	ResponseURL 		string
+	RepsonseMethod		string
+	ResponseDuration	time.Duration
+	ResponseTime		time.Time
+	ResponseSeconds     float64
+	ResponseSince       *TimeSpan
+}
 
 type Stats struct {
 	mu                  sync.RWMutex
@@ -14,7 +32,14 @@ type Stats struct {
 	Pid                 int
 	ResponseCounts      map[string]int
 	TotalResponseCounts map[string]int
+	URLRequestLatency   map[string]int
+	URLRequestCounts    map[string]int
 	TotalResponseTime   time.Time
+	RequestTypeCounts	map[string]int
+	UserAgentCounts		map[string]int
+	URLHighestResponse  map[string]float64
+	URLLowestResponse   map[string]float64
+	MaxResponseTime		*ResponseURL
 }
 
 func New() *Stats {
@@ -23,7 +48,14 @@ func New() *Stats {
 		Pid:                 os.Getpid(),
 		ResponseCounts:      map[string]int{},
 		TotalResponseCounts: map[string]int{},
+		URLRequestCounts:    map[string]int{},
+		URLRequestLatency:   map[string]int{},
 		TotalResponseTime:   time.Time{},
+		RequestTypeCounts:	 map[string]int{},
+		UserAgentCounts:	 map[string]int{},
+		URLHighestResponse:  map[string]float64{},
+		URLLowestResponse:   map[string]float64{},
+		MaxResponseTime:     &ResponseURL{ ResponseURL: "", ResponseDuration: 0, ResponseTime: time.Time{}, ResponseSeconds: 0.0, ResponseSince: &TimeSpan{} },
 	}
 
 	go func() {
@@ -44,13 +76,13 @@ func (mw *Stats) ResetResponseCounts() {
 }
 
 // MiddlewareFunc makes Stats implement the Middleware interface.
-func (mw *Stats) Handler(h http.Handler) http.Handler {
+func (mw *Stats) Handler( c *web.C, h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		beginning, recorder := mw.Begin(w)
 
 		h.ServeHTTP(recorder, r)
 
-		mw.End(beginning, recorder)
+		mw.End(beginning, recorder, r.URL.String(), r.Method, r.UserAgent() )
 	})
 }
 
@@ -60,18 +92,18 @@ func (mw *Stats) ServeHTTP(w http.ResponseWriter, r *http.Request, next http.Han
 
 	next(recorder, r)
 
-	mw.End(beginning, recorder)
+	mw.End(beginning, recorder, r.URL.String(), r.Method, r.UserAgent() )
 }
 
-func (mw *Stats) Begin(w http.ResponseWriter) (time.Time, ResponseWriter) {
+func (mw *Stats) Begin(w http.ResponseWriter) (time.Time, Recorder) {
 	start := time.Now()
 
-	writer := NewRecorderResponseWriter(w, 200)
+	writer := &RecorderResponseWriter{w, 200, 0}
 
 	return start, writer
 }
 
-func (mw *Stats) EndWithStatus(start time.Time, status int) {
+func (mw *Stats) EndWithStatus(start time.Time, status int, url string, method string, useragent string ) {
 	end := time.Now()
 
 	responseTime := end.Sub(start)
@@ -84,27 +116,49 @@ func (mw *Stats) EndWithStatus(start time.Time, status int) {
 
 	mw.ResponseCounts[statusCode]++
 	mw.TotalResponseCounts[statusCode]++
-	mw.TotalResponseTime = mw.TotalResponseTime.Add(responseTime)
+	mw.TotalResponseTime = mw.TotalResponseTime.Add( responseTime )
+	mw.URLRequestCounts[url]++
+	mw.URLRequestLatency[url] += int( responseTime )
+	mw.RequestTypeCounts[method]++
+	mw.UserAgentCounts[useragent]++
+
+	if mw.MaxResponseTime.ResponseDuration < responseTime {
+		mw.MaxResponseTime.ResponseDuration = responseTime
+		mw.MaxResponseTime.ResponseURL = url
+		mw.MaxResponseTime.ResponseTime = time.Now()
+		mw.MaxResponseTime.RepsonseMethod = method
+	}
+
+	if mw.URLHighestResponse[url] < float64( responseTime ) { mw.URLHighestResponse[url] = float64( responseTime )}
+	if mw.URLLowestResponse[url] == float64( 0 ) { mw.URLLowestResponse[url] = float64( responseTime )}
+	if float64( responseTime ) < mw.URLLowestResponse[url] { mw.URLLowestResponse[url] = float64( responseTime )}
 }
 
-func (mw *Stats) End(start time.Time, recorder ResponseWriter) {
-	mw.EndWithStatus(start, recorder.Status())
+func (mw *Stats) End(start time.Time, recorder Recorder, url string, method string, useragent string ) {
+	mw.EndWithStatus(start, recorder.Status(), url, method, useragent )
 }
 
 type data struct {
-	Pid                    int            `json:"pid"`
-	UpTime                 string         `json:"uptime"`
-	UpTimeSec              float64        `json:"uptime_sec"`
-	Time                   string         `json:"time"`
-	TimeUnix               int64          `json:"unixtime"`
-	StatusCodeCount        map[string]int `json:"status_code_count"`
-	TotalStatusCodeCount   map[string]int `json:"total_status_code_count"`
-	Count                  int            `json:"count"`
-	TotalCount             int            `json:"total_count"`
-	TotalResponseTime      string         `json:"total_response_time"`
-	TotalResponseTimeSec   float64        `json:"total_response_time_sec"`
-	AverageResponseTime    string         `json:"average_response_time"`
-	AverageResponseTimeSec float64        `json:"average_response_time_sec"`
+	Pid                    int            `xml:"pid,attr" json:"pid"`
+	UpTime                 string         `xml:"uptime,attr" json:"uptime"`
+	UpTimeSec              float64        `xml:"uptime_sec,attr" json:"uptime_sec"`
+	Time                   string         `xml:"time,attr" json:"time"`
+	TimeUnix               int64          `xml:"unixtime,attr" json:"unixtime"`
+	StatusCodeCount        map[string]int `xml:"status_code_count" json:"status_code_count"`
+	TotalStatusCodeCount   map[string]int `xml:"total_status_code_count" json:"total_status_code_count"`
+	Count                  int            `xml:"count,attr" json:"count"`
+	TotalCount             int            `xml:"total_count,attr" json:"total_count"`
+	TotalResponseTime      string         `xml:"total_response_time,attr" json:"total_response_time"`
+	TotalResponseTimeSec   float64        `xml:"total_response_time_sec,attr" json:"total_response_time_sec"`
+	AverageResponseTime    string         `xml:"average_response_time,attr" json:"average_response_time"`
+	AverageResponseTimeSec float64        `xml:"average_response_time_sec,attr" json:"average_response_time_sec"`
+	URLRequestCounts	   map[string]int `xml:"URLRequestCounts" json:"URLRequestCounts"`
+	RequestTypeCounts	   map[string]int `xml:"RequestTypeCounts" json:"RequestTypeCounts"`
+	UserAgentCounts	   	   map[string]int `xml:"UserAgentCounts" json:"UserAgentCounts"`
+	URLRequestLatency      map[string]int `xml:"URLRequestLatency" json:"URLRequestLatency"`
+	URLHighestResponse     map[string]float64 `xml:"URLHighestResponse" json:"URLHighestResponse"`
+	URLLowestResponse      map[string]float64 `xml:"URLLowestResponse" json:"URLLowestResponse"`
+	MaxResponseTime	   	   *ResponseURL   `xml:"MaxResponseTimes" json:"MaxResponseTimes"`
 }
 
 func (mw *Stats) Data() *data {
@@ -133,6 +187,17 @@ func (mw *Stats) Data() *data {
 		averageResponseTime = time.Duration(avgNs)
 	}
 
+	mw.MaxResponseTime.ResponseSeconds = mw.MaxResponseTime.ResponseDuration.Seconds()
+
+    duration := time.Since( mw.MaxResponseTime.ResponseTime )
+    weeks, days, hours, minutes, seconds := SecondsToDate( Round( duration.Seconds(), 0 ))
+	mw.MaxResponseTime.ResponseSince.Weeks = weeks
+	mw.MaxResponseTime.ResponseSince.Days = days
+	mw.MaxResponseTime.ResponseSince.Hours = hours
+	mw.MaxResponseTime.ResponseSince.Minutes = minutes
+	mw.MaxResponseTime.ResponseSince.Seconds = seconds
+
+
 	r := &data{
 		Pid:                    mw.Pid,
 		UpTime:                 uptime.String(),
@@ -147,9 +212,35 @@ func (mw *Stats) Data() *data {
 		TotalResponseTimeSec:   totalResponseTime.Seconds(),
 		AverageResponseTime:    averageResponseTime.String(),
 		AverageResponseTimeSec: averageResponseTime.Seconds(),
+		URLRequestCounts:       mw.URLRequestCounts,
+		RequestTypeCounts:		mw.RequestTypeCounts,
+		UserAgentCounts:		mw.UserAgentCounts,
+		URLRequestLatency:      mw.URLRequestLatency,
+		URLHighestResponse:     mw.URLHighestResponse,
+		URLLowestResponse:      mw.URLLowestResponse,
+		MaxResponseTime:		mw.MaxResponseTime,
 	}
 
 	mw.mu.RUnlock()
 
 	return r
+}
+
+func SecondsToDate( seconds2 float64 ) ( int, int, int, int, int ) {
+	seconds := int( seconds2 )
+	weeks := seconds / (7*24*60*60)
+	days := seconds / (24*60*60) - 7*weeks
+	hours := seconds / (60*60) - 7*24*weeks - 24*days
+	minutes := seconds / 60 - 7*24*60*weeks - 24*60*days - 60*hours
+	seconds3 := seconds - 7*24*60*60*weeks - 24*60*60*days - 60*60*hours - 60*minutes
+
+	return weeks, days, hours, minutes, seconds3
+}
+
+func Round(v float64, decimals int) float64 {
+     var pow float64 = 1
+     for i:=0; i<decimals; i++ {
+         pow *= 10
+     }
+     return float64(int((v * pow) + 0.5)) / pow
 }
